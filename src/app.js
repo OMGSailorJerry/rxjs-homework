57ff4bc055d30acacce7f5177082b423c86e8ccf
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-06 22:23:29 +0200

    Opearatos like map and throttleTime

--- src/app.js
+++ src/app.js
@@ -4,35 +4,16 @@ import Rx from 'rxjs/Rx';
 
 let button = document.querySelector('button');
 
+let observable = Rx.Observable.interval(1000);
+
 let observer = {
-    next: function(value) {
+    next: (value) => {
         console.log(value);
-    },
-    error: function(error) {
-        console.log(error);
-    },
-    complete: function() {
-        console.log('Completed');
     }
-}
+};
 
-// Rx.Observable.fromEvent(button, 'click')
-let subscription = Rx.Observable.create(function(obs) {
-    // obs.next('A value');
-    // obs.error('Error');
-    // setTimeout(() => {
-    //     obs.complete();
-    // }, 2000);
-    // obs.next('A second value');
-    
-    button.onclick = (event) => {
-        obs.next(event);
-    };
+observable.map((value) => {
+    return `Number is - ${value * 2}`;
 })
+.throttleTime(1000)
 .subscribe(observer);
-
-setTimeout(() => {
-    subscription.unsubscribe();
-}, 5000);
-
-    
