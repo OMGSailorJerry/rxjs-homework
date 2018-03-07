c4a0e5b1c1984df37d4eb1fb86ca47b717a9ca33
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-07 19:48:51 +0200

    Filter

--- src/app.js
+++ src/app.js
@@ -2,3 +2,20 @@
 
 import Rx from 'rxjs/Rx';
 
+let myObservable = Rx.Observable.interval(1000);
+
+let myObserver = {
+    next: (value) => {
+        console.log(value);
+    },
+    error: (error) => {
+        console.log('Error: ', error);
+    }
+};
+
+myObservable
+    .filter((value) => {
+      return value % 2 === 0;  
+    })
+    .subscribe(myObserver);
+
