272229abad155bc9d10374cee383612fb3e8f1d3
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-08 16:13:56 +0200

    Reduce vs scan

--- src/app.js
+++ src/app.js
@@ -4,14 +4,20 @@ import Rx from 'rxjs/Rx';
 
 let input = document.querySelector('input');
 
-let myObservable = Rx.Observable.fromEvent(input, 'input');
+let myObservable = Rx.Observable.of(1, 2, 3, 4, 5);
 
 let myObserver = {
     next: (event) => {
-        console.log(event.target.value);
+        console.log(event);
     }
 };
 
 myObservable
+    .scan((total, currentValue) => {
+        return total + currentValue;
+    }, 0)
+    // .reduce((total, currentValue) => {
+    //     return total + currentValue;
+    // }, 0)
     .subscribe(myObserver);
 
