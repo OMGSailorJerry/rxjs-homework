40680efe8f22b6191c6323cac3dd650b31cc2ba1
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-08 19:33:01 +0200

    BehaviorSubject

--- src/app.js
+++ src/app.js
@@ -3,3 +3,11 @@
 import Rx from 'rxjs/Rx';
 
 let button = document.querySelector('button');
+let div = document.querySelector('div');
+
+let buttonClickObs = Rx.Observable.fromEvent(button, 'click');
+
+let clickEmmited = new Rx.BehaviorSubject('Not Clicked!');
+
+buttonClickObs.subscribe(event => clickEmmited.next(`${event.target.nodeName} was clicked!`));
+clickEmmited.subscribe(value => {div.textContent = value});
