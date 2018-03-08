bfd57983abf9fbd5a32bf8e90dc5475de70fd4c3
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-08 19:15:08 +0200

    switchmMap end

--- src/app.js
+++ src/app.js
@@ -3,12 +3,3 @@
 import Rx from 'rxjs/Rx';
 
 let button = document.querySelector('button');
-
-let myObservable1 = Rx.Observable.fromEvent(button, 'click');
-let myObservable2 = Rx.Observable.interval(500);
-
-let observer2 = (value) => console.log(value);
-
-myObservable1
-    .switchMap(() => myObservable2)
-    .subscribe(observer2);
