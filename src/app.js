c7ccfab7b2e4fe2291970eb531cccf4dfb9888eb
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-08 18:38:37 +0200

    mergeMap()

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@
 
 import Rx from 'rxjs/Rx';
 
-let input = document.querySelector('input');
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+let span = document.querySelector('span');
 
-let myObservable = Rx.Observable.fromEvent(input, 'input');
+let myObservable1 = Rx.Observable.fromEvent(input1, 'input');
+let myObservable2 = Rx.Observable.fromEvent(input2, 'input');
 
 let myObserver = {
-    next: (value) => {
-        console.log(value);
+    next: value => {
+        span.textContent = value;
     }
 };
 
-myObservable
-    .pluck('target', 'value')
+myObservable1.mergeMap(
+    event1 => {
+        return myObservable2.map(event2 => {
+            return `${event1.target.value} ${event2.target.value}`;
+        });
+    })
     .subscribe(myObserver);
-
- 
